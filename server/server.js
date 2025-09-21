9a0de0a8de5ef593f2cda59d77195e84fcc69e22
Atul Himanshu <priyanshuprogrammer49@gmail.com>
2025-09-21 18:59:44 +0530

    start v1 serverbasic code added

--- server/server.js
+++ server/server.js
@@ -1,1 +1,33 @@
-// hi i am atul hi bhai
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import connectDB from './config/db.js';
+import userRoutes from './routes/userRoutes.js';
+import productRoutes from './routes/productRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+// Connect to MongoDB
+connectDB(MONGO_URI);
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
+app.use('/api/users', userRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/orders', orderRoutes);
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
